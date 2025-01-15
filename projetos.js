dbc98873c0b44b775f8c70ab3e62e48bbf06a2b9
Gokan <138010470+GokanZin@users.noreply.github.com>
2025-01-15 19:45:51 -0300

    Create projetos.js

---
+++ projetos.js
@@ -0,0 +1,17 @@
+const items = [
+    {
+      title: "Plugin teste",
+      tags: ["MySQL", "SQLite", "Spigot"],
+      video: "https://www.youtube.com/embed/YoNpgc9lz2o"
+    },
+    {
+      title: "Outro Plugin",
+      tags: ["Java", "Bukkit", "API"],
+      video: "https://www.youtube.com/embed/dQw4w9WgXcQ"
+    },
+    {
+      title: "Outro Plugin",
+      tags: ["Java", "Bukkit", "API"],
+      video: "https://www.youtube.com/embed/dQw4w9WgXcQ"
+    }
+  ];
