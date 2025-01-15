5c39955c269678c82468ac4478bf716eed83f6a3
Gokan <138010470+GokanZin@users.noreply.github.com>
2025-01-15 20:04:51 -0300

    Update projetos.js

--- projetos.js
+++ projetos.js
@@ -1,6 +1,6 @@
 const items = [
     {
-      title: "Plugin MClasse",
+      title: "Plugin mClass",
       tags: ["MySQL", "SQLite", "Spigot"],
       video: "https://www.youtube.com/embed/cJFlNaoP1PU"
     }
