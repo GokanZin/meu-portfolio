f0dcbd3fbb2f901667bacfb2d60933a820032785
Gokan <138010470+GokanZin@users.noreply.github.com>
2025-01-15 23:19:56 -0300

    Update projetos.js

--- projetos.js
+++ projetos.js
@@ -3,5 +3,28 @@ const items = [
       title: "Plugin mClass",
       tags: ["MySQL", "SQLite", "Spigot"],
       video: "https://www.youtube.com/embed/cJFlNaoP1PU"
-    }
+    },
+    {
+      title: "Plugin mCombateMode (1.7.10 implemente dbc/nc)",
+      tags: ["Forge", "Spigot", "NPCAPI"],
+      video: "https://www.youtube.com/embed/k4LBB7MNcJw"
+    },
+    {
+      title: "Plugin mClans (1.7.10)",
+      tags: ["Forge", "Spigot", "NPCAPI"],
+      video: "https://www.youtube.com/embed/x4Jf-IZD2UE"
+    },
+
+    {
+      title: "Plugin mOrbC (1.7.10)",
+      tags: ["SQLite", "Spigot"],
+      video: "https://www.youtube.com/embed/uL1jxyRksB0"
+    },
+
+    {
+      title: "mFruit (Mod 1.16.5)",
+      tags: ["Forge"],
+      video: "https://www.youtube.com/embed/73MuHlwKTSc"
+    },
+
   ];
